6cdb94ecdc245198c85f251f08fd0c6c425e499c
Himmat Singh <himmsrathore@hotmail.com>
2025-01-01 13:34:03 +0530

    d

---
+++ index.php
@@ -0,0 +1,172 @@
+# Hi there, I'm Himms 👋
+
+<img align="right" alt="" src="backend-show.png" width="350" height="350" />
+
+## My Backend Engineering Show
+- [The Backend Engineering Show with Saad Hassan](https://www.youtube.com/playlist?list=PLx8CItc_kq60ORFN7NVaD2xae_WystDOa)
+
+### I'm a Developer, Teacher, and Researcher!
+- 🔭 I’m working as a Backend and infra Engineer with Typescript and Python.
+- 🌱 I’m currently learning Spring Boot & Go.
+- 👯 I’m looking to collaborate with other content creators and developers.
+- 📢 Love to teach and inspire students. Taught more than 2k students.
+- 🥅 2024 Goals: Contribute more to Open Source projects.
+
+<h2><i>💻 Tech Stack and Tools</i></h2>
+
+<table width="100">
+<tr>
+    <td align='center' width="190">
+        <img src="https://github.com/abranhe/programming-languages-logos/blob/master/src/javascript/javascript.svg" width="60">
+    </td>
+    <td align='center' width="190">
+        <img src="https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg">
+    </td>
+    <td align='center' width="190">
+        <img src="https://static.vecteezy.com/system/resources/previews/012/697/295/non_2x/3d-python-programming-language-logo-free-png.png"  width="60" height="60">
+    </td>
+     <td align='center' width="190">
+        <img src="https://git-scm.com/images/logos/1color-darkbg@2x.png" width="100">
+    </td>
+    <td align='center' width="190">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Kubernetes_logo_without_workmark.svg/2109px-Kubernetes_logo_without_workmark.svg.png" height="80">
+    </td>
+</tr>
+<tr>
+    <td align='center'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Prometheus_software_logo.svg/2066px-Prometheus_software_logo.svg.png" height="80">
+    </td>
+    <td align='center'>
+        <img src="https://www.vectorlogo.zone/logos/nodejs/nodejs-ar21.svg">
+    </td>
+    <td align='center'>
+        <img src="https://vegibit.com/wp-content/uploads/2018/05/expressjs.png">
+    </td>
+    <td align='center'>
+        <img src="https://www.skillfinder.com.au/media/wysiwyg/mongodb-logo-skill-finder.png">
+    </td>
+    <td align='center'>
+        <img src="https://www.vectorlogo.zone/logos/firebase/firebase-ar21.svg">
+    </td>
+</tr>
+<tr>
+    <td align='center'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/600px-HTML5_Badge.svg.png" height="60" width="60">
+    </td>
+    <td align='center'>
+        <img src="https://raw.githubusercontent.com/devicons/devicon/0d6c64dbbf311879f7d563bfc3ccf559f9ed111c/icons/css3/css3-original-wordmark.svg" width="60" height="60">
+    </td>
+    <td align='center'>
+        <img src="https://miro.medium.com/v2/resize:fit:1400/1*4M4OghuybPhjRsLxhrNsGA.png">
+    </td>
+    <td align='center'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png" width="60">
+    </td>
+    <td align='center'>
+        <img src="https://allvectorlogo.com/img/2019/06/elastic-elasticsearch-logo-vector.png">
+    </td>
+</tr>
+<tr>
+    <td align='center'>
+        <img src="https://download.logo.wine/logo/PostgreSQL/PostgreSQL-Logo.wine.png">
+    </td>
+    <td align='center'>
+        <img src="https://download.logo.wine/logo/MySQL/MySQL-Logo.wine.png" >
+    </td>
+    <td align='center'>
+        <img src="https://miro.medium.com/max/901/1*GkrYGz_r9W6AVgEloQpJFQ.png">
+    </td>
+    <td align='center'>
+        <img src="https://download.logo.wine/logo/Redis/Redis-Logo.wine.png">
+    </td>
+    <td align='center'>
+        <img src="https://cdn-images-1.medium.com/max/1200/1*kqpVTzo8b0e2oKdOjWQxZA.png">
+    </td>
+</tr>
+<tr>
+    <td align='center'>
+        <img src="https://www.djangoproject.com/m/img/logos/django-logo-negative.png">
+    </td>
+    <td align='center'>
+        <img src="https://buttercms.com/static/images/tech_banners/Flask.png" >
+    </td>
+    <td align='center'>
+        <img src="https://logodix.com/logo/1686380.png" width="60" height="60">
+    </td>
+    <td align='center'>
+        <img src="https://logos-download.com/wp-content/uploads/2016/10/Ansible_logo.png" width="60" height="60">
+    </td>
+    <td align='center'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Jenkins_logo.svg/1200px-Jenkins_logo.svg.png" height="80">
+    </td>
+</tr>
+
+<tr>
+    <td align='center'>
+        <img src="https://logos-world.net/wp-content/uploads/2021/08/Amazon-Web-Services-AWS-Emblem.png">
+    </td>
+    <td align='center'>
+        <img src="https://download.logo.wine/logo/Microsoft_Azure/Microsoft_Azure-Logo.wine.png">
+    </td>
+    <td align='center'>
+        <img src="https://cloudnesil.com/wp-content/uploads/2019/05/serverless.png">
+    </td>
+    <td align='center'>
+        <img src="https://www.docker.com/wp-content/uploads/2022/03/vertical-logo-monochromatic.png" height="80" width="90" >
+    </td>
+    <td align='center'>
+        <img src="https://www.nginx.com/wp-content/uploads/2018/08/NGINX-logo-rgb-large.png" >
+    </td>
+</tr>
+    
+</table>
+
+<br />
+<br />
+
+## 🤝🏻 &nbsp;Connect with Me
+
+<p align="center">
+<a href="https://www.saad-hassan.com"><img src="https://img.shields.io/badge/-saadhassan.com-3423A6?style=flat&logo=Google-Chrome&logoColor=white"/></a>
+<a href="https://linkedin.com/in/saad-haxxan"><img src="https://img.shields.io/badge/-Saad%20Hassan-0077B5?style=flat&logo=Linkedin&logoColor=white"/></a>
+<a href="mailto:contact@saad-hassan.com"><img src="https://img.shields.io/badge/-contact@saadhassan.com-D14836?style=flat&logo=Gmail&logoColor=white"/></a>
+<a href="https://instagram.com/saadhaxxan"><img src="https://img.shields.io/badge/-@saadhaxxan-E4405F?style=flat&logo=Instagram&logoColor=white"/></a>
+<a href="https://facebook.com/saaadhaxxan"><img src="https://img.shields.io/badge/-@saaadhaxxan-1877F2?style=flat&logo=Facebook&logoColor=white"/></a>
+
+## Some Community Projects
+
+|      Project :octocat:   |     Issues :bug:   | Open PRs :bell:  | Closed PRs :fire:  |
+|-------------|-------------------|---|---|
+| [**Awesome Python Projects**](https://github.com/saadhaxxan/Awesome-Python-Projects) | [![GitHub issues](https://img.shields.io/github/issues/saadhaxxan/Awesome-Python-Projects?color=green&logo=github&style=flat)](https://github.com/saadhaxxan/Awesome-Python-Projects/issues) | [![GitHub PRs](https://img.shields.io/github/issues-pr/saadhaxxan/Awesome-Python-Projects?style=flat&logo=github)](https://github.com/saadhaxxan/Awesome-Python-Projects/pulls)  | [![GitHub PRs](https://img.shields.io/github/issues-pr-closed/saadhaxxan/Awesome-Python-Projects?style=flat&color=critical&logo=github)](https://github.com/saadhaxxan/Awesome-Python-Projects/pulls?q=is%3Apr+is%3Aclosed)  |
+| [**Learn_Machine_Learning_in_5_Months**](https://github.com/saadhaxxan/Learn_Machine_Learning_in_5_Months/) | [![GitHub issues](https://img.shields.io/github/issues/saadhaxxan/Learn_Machine_Learning_in_5_Months?color=green&logo=github&style=flat)](https://github.com/saadhaxxan/Learn_Machine_Learning_in_5_Months/issues) | [![GitHub PRs](https://img.shields.io/github/issues-pr/saadhaxxan/Learn_Machine_Learning_in_5_Months?style=flat&logo=github)](https://github.com/saadhaxxan/Learn_Machine_Learning_in_5_Months/pulls)  | [![GitHub PRs](https://img.shields.io/github/issues-pr-closed/saadhaxxan/Learn_Machine_Learning_in_5_Months?style=flat&color=critical&logo=github)](https://github.com/saadhaxxan/Learn_Machine_Learning_in_5_Months/pulls?q=is%3Apr+is%3Aclosed)   |
+| [**Python For Ethical Hacking**](https://github.com/saadhaxxan/Python-For-Ethical-Hacking/) | [![GitHub issues](https://img.shields.io/github/issues/saadhaxxan/Python-For-Ethical-Hacking?color=green&logo=github&style=flat)](https://github.com/saadhaxxan/Python-For-Ethical-Hacking/issues) | [![GitHub PRs](https://img.shields.io/github/issues-pr/saadhaxxan/Python-For-Ethical-Hacking?style=flat&logo=github)](https://github.com/saadhaxxan/Python-For-Ethical-Hacking/pulls)  | [![GitHub PRs](https://img.shields.io/github/issues-pr-closed/saadhaxxan/Python-For-Ethical-Hacking?style=flat&color=critical&logo=github)](https://github.com/saadhaxxan/Python-For-Ethical-Hacking/pulls?q=is%3Apr+is%3Aclosed)   |
+| [**Learn-Data-Science-in-3-months**](https://github.com/saadhaxxan/Learn-Data-Science-in-3-months) | [![GitHub issues](https://img.shields.io/github/issues/saadhaxxan/Learn-Data-Science-in-3-months?color=green&logo=github&style=flat)](https://github.com/saadhaxxan/Learn-Data-Science-in-3-months/issues) | [![GitHub PRs](https://img.shields.io/github/issues-pr/saadhaxxan/Learn-Data-Science-in-3-months?style=flat&logo=github)](https://github.com/saadhaxxan/Learn-Data-Science-in-3-months/pulls)  | [![GitHub PRs](https://img.shields.io/github/issues-pr-closed/saadhaxxan/Learn-Data-Science-in-3-months?style=flat&color=critical&logo=github)](https://github.com/saadhaxxan/Learn-Data-Science-in-3-months/pulls?q=is%3Apr+is%3Aclosed)   |
+| [**Learn Blockchain in 3 months**](https://github.com/saadhaxxan/Learn-Blockchain-in-3-months) | [![GitHub issues](https://img.shields.io/github/issues/saadhaxxan/Learn-Blockchain-in-3-months?color=green&logo=github&style=flat)](https://github.com/saadhaxxan/Learn-Blockchain-in-3-months/issues) | [![GitHub PRs](https://img.shields.io/github/issues-pr/saadhaxxan/Learn-Blockchain-in-3-months?style=flat&logo=github)](https://github.com/saadhaxxan/Learn-Blockchain-in-3-months/pulls)  | [![GitHub PRs](https://img.shields.io/github/issues-pr-closed/saadhaxxan/Learn-Blockchain-in-3-months?style=flat&color=critical&logo=github)](https://github.com/saadhaxxan/Learn-Blockchain-in-3-months/pulls?q=is%3Apr+is%3Aclosed)   |
+    
+## My Activities :mailbox_with_mail:
+
+| BOOTCAMPS & SESSIONS :tv: | 
+| :--- |
+| [Deep Learning with Tensorflow ](https://github.com/dscuol/Deep-Learning-Course) |
+| [Machine Learning Course](https://github.com/dscuol/Machine-Learning-Course) |
+| [Web Development Bootcamp](https://github.com/dscuol/Web-Development-Bootcamp) | 
+| [Advanced Web Development Bootcamp](https://github.com/dscuol/Advanced-web-development-series) | 
+| [NLP Bootcamp](https://github.com/dscuol/Natural-Language-Processing-Bootcamp) | 
+| [DSA Bootcamp C++](https://github.com/saadhaxxan/DSA-Bootcamp-Cpp) | 
+
+<h2><i>⚙️ GitHub Analytics</i></h2>
+
+<p align="center">
+<a href="https://github.com/saadhaxxan">
+  <img height="180em"  src="https://github-readme-stats-eight-theta.vercel.app/api/top-langs/?username=saadhaxxan&layout=compact&langs_count=8&theme=algolia"/>
+</a>
+  <img height="180em" src="https://github-readme-streak-stats.herokuapp.com/?user=saadhaxxan&show_icons=true&locale=en&layout=demo&theme=merko&hide_border=true" />
+</p>
+
+# Github Metrics
+<p align="center">
+	<img width="625em" src="https://github.com/saadhaxxan/saadhaxxan/blob/master/github-metrics.svg" />
+</p>
+#   h i m m s r a t h o r e  
+ #   h i m m s r a t h o r e  
+ 
